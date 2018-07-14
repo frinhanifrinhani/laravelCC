8f59bf8ffca8d578c621ea48d795dd0be59f25e8
Frinhani <frinhani.frinhani@gmail.com>
2018-07-14 00:18:55 -0300

    Controller: ProdutoController@lista

--- routes/web.php
+++ routes/web.php
@@ -3,14 +3,4 @@ Route::get('/',function(){
 	return '<h1>Primeira lógica com Laravel</h1>';
 });
 
-Route::get('outralogica',function(){
-	return '<h1>Outra lógica com Laravel</h1>';
-});
-
-Route::get('testerota',function(){
-	return 'teste rota';
-});
-
-Route::get('testerota/comid',function(){
-	return 'teste rota comid';
-});
+Route::get('/produtos','ProdutoController@lista');
