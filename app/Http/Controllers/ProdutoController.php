4b629904a16d94db4202fa34d06a2deaafe25687
Frinhani <frinhani.frinhani@gmail.com>
2018-07-14 21:12:06 -0300

    Teste conexão banco de dados

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -1,11 +1,21 @@
 <?php
 	
 namespace estoque\Http\Controllers;
+use Illuminate\Support\Facades\DB;
 
 class ProdutoController extends Controller{
 	
 	public function lista(){
-		return '<h1>Listagem de produtos com Laravel</h1>';
+		$html = '<h1>Listagem de produtos com Laravel</h1>';
+		$produtos = DB::select('select * from estoque_laravel.produtos');
+
+		$html .= '<ul>';
+			foreach ($produtos as $p){
+				$html .= '<li> Nome: '. $p->nome . ', Descrição: '. $p->descricao .'</li>';
+			}
+		$html .= '</ul>';
+
+		return $html;
 	}
 }
 
