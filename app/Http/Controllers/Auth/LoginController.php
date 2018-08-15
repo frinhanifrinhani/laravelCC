0608de1fc496051ab28793d7ae7b7a2d060e273f
frinhani.frinhani <frinhani.frinhani@gmail.com>
2018-08-14 23:53:43 -0300

    Logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,10 +36,5 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-
-    public function showLoginForm()
-    {
-        return view('auth.login');
-    }
-
+    
 }
